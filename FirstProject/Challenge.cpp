98fa9e4c93bd0c5b86aef4ec2d3466354fb408f1
Black Kitten <blackkitten010@gmail.com>
2020-08-13 12:33:20 +0700

    .

--- FirstProject/Challenge.cpp
+++ FirstProject/Challenge.cpp
@@ -1,9 +1,32 @@
 #include<stdio.h>
+#include<math.h>
 int main() {
-	int x, y,i;
+	int x, y,i,l;
+	float a=0,s=0,o;
 	scanf_s("%d %d", &x, &y);
-	for (i = x; i <= y; i++) {
-		printf("%d ", i);
+	if (x < y) {
+		for (i = x; i <= y; i++) {
+			printf("%d ", i);
+			a += i;
+		}
+		o = a / ((y - x) + 1);
+		printf("\nAverage = %.1f", o);
+		for (l = x; l <= y; l++) {
+			s += (l - o) * (l - o);
+		}
+		printf("\nSD = %.2f", sqrt(s / (y - x)));
+	}
+	else if (x >= y) {
+		for (i = x; i >= y; i--) {
+			printf("%d ", i);
+			a += i;
+		}
+		o = a / ((x - y) + 1);
+		printf("\nAverage = %.1f", o);
+		for (l = x; l >= y; l--) {
+			s += (l - o) * (l - o);
+		}
+		printf("\nSD = %.2f", sqrt(s / (x - y)));
 	}
 	return 0;
 }
